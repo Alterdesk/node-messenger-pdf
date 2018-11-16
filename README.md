316a769225b2732f01246bf497fc2d4d6d732f80
jurriaan <jurriaan@alterdesk.com>
2018-11-16 08:41:35 +0100

    Able to remove unwanted html tags and use header and footer

--- README.md
+++ README.md
@@ -6,7 +6,20 @@ Helper module to generate PDF files from HTML with a headless Chrome/Chromium in
 Start an instance of Chrome or Chromium that will receive the commands on the given port(default is 9222).
 Replace "chromium-browser" with your installed chrome/chromium binary.
 ```bash
-chromium-browser --headless --disable-gpu --remote-debugging-port=9222
+chromium-browser --headless \
+--disable-gpu \
+--interpreter none \
+--disable-translate \
+--disable-extensions \
+--disable-background-networking \
+--safebrowsing-disable-auto-update \
+--disable-sync \
+--metrics-recording-only \
+--disable-default-apps \
+--no-first-run \
+--mute-audio \
+--hide-scrollbars \
+--remote-debugging-port=9222
 ```
 
 ## Usage
@@ -41,6 +54,28 @@ body {
     -webkit-print-color-adjust:exact;
 }
 ```
+
+## Sandbox issues
+If you are unable to run the headless chrome/chromium instance without using --no-sandbox, you can try the following 
+configurations. The configurations were tested on CentOS successfully.
+
+```bash
+# Run as root
+echo "user.max_user_namespaces=15000" >> /etc/sysctl.conf
+```
+Reboot your machine and check if the configuration value 15000 was set:
+```bash
+sudo sysctl -a | grep user.max_user_namespaces
+```
+Try if running in with a sandbox now works, if the problem persists because you are using an older kernel:
+```bash
+grubby --args="user_namespace.enable=1 namespace.unpriv_enable=1" --update-kernel="$(grubby --default-kernel)"
+```
+Reboot again and check if the changes are set:
+```bash
+cat /proc/cmdline
+```
+When the values are set the headless chrome/chromium instance should run in a sandbox.
 
 ## Environment variables
 Chrome/Chromium port to connect to
--- pdf.js
+++ pdf.js
@@ -1,4 +1,5 @@
 const HtmlPdf = require('html-pdf-chrome');
+const Parse5 = require('parse5');
 const UuidV1 = require('uuid/v1');
 const FileSystem = require('fs');
 const Mkdirp = require('mkdirp');
@@ -10,12 +11,14 @@ var tmpDir = Path.resolve(OS.tmpdir(), 'messenger-pdfs');
 
 var logger = new Log(process.env.NODE_MESSENGER_SDK_LOG_LEVEL || process.env.HUBOT_LOG_LEVEL || 'info');
 
-const options = {
-    port: process.env.NODE_MESSENGER_PDF_PORT || 9222  // Chrome/Chromium port
-};
+const port = process.env.NODE_MESSENGER_PDF_PORT || 9222;  // Chrome/Chromium port
 
 class Pdf {
-    create(filename, html, callback) {
+    constructor() {
+        this.removeTags = ["script", "iframe"];
+    }
+
+    create(filename, html, header, footer, callback) {
         if(!filename || filename === "") {
             logger.error("Pdf::create() Filename invalid: " + filename);
             callback(false, null);
@@ -23,6 +26,17 @@ class Pdf {
         if(!html || html === "") {
             logger.error("Pdf::create() HTML invalid: " + html);
             callback(false, null);
+        }
+
+        if(this.removeTags && this.removeTags.length > 0) {
+            try {
+                var document = Parse5.parse(html);
+                this.removeHtmlNode(document, this.removeTags);
+                html = Parse5.serialize(document);
+            } catch(err) {
+                logger.error("Pdf::create() Unable to remove tags from HTML: ", err);
+                callback(false, null);
+            }
         }
 
         var tmpDirPath = tmpDir + "/" + UuidV1();
@@ -36,6 +50,22 @@ class Pdf {
 
             var filePath = Path.resolve(tmpDirPath, filename);
 
+            var options = {};
+            options["port"] = port;
+            if((header && header.length > 0) || (footer && footer.length > 0)) {
+                var printOptions = {};
+                printOptions["displayHeaderFooter"] = true;
+                if(header && header.length > 0) {
+                        logger.debug("Pdf::create() Using header");
+                    printOptions["headerTemplate"] = header;
+                }
+                if(footer && footer.length > 0) {
+                        logger.debug("Pdf::create() Using footer");
+                    printOptions["footerTemplate"] = footer;
+                }
+                options["printOptions"] = printOptions;
+            }
+
             HtmlPdf.create(html, options).then((pdf) => {
                 var promise = pdf.toFile(filePath);
                 promise.then((res) => {
@@ -48,6 +78,35 @@ class Pdf {
                 });
             });
         });
+    }
+
+    removeHtmlNode(node, removeTags) {
+        if(!node) {
+            return true;
+        }
+        var name = node.tagName || node.nodeName;
+        if(!name) {
+            return true;
+        }
+        if(removeTags.indexOf(name) !== -1) {
+            logger.error("Pdf::removeHtmlNode() Removing tag \"" + name + "\" with contents \"" + Parse5.serialize(node) + "\"");
+            return true;
+        }
+        if(node.childNodes && node.childNodes.length > 0) {
+            var removeNodes = [];
+            for(let index in node.childNodes) {
+                var child = node.childNodes[index];
+                if(this.removeHtmlNode(child, removeTags)) {
+                    removeNodes.push(child);
+                }
+            }
+            for(let index in removeNodes) {
+                var removeNode = removeNodes[index];
+                let i = node.childNodes.indexOf(removeNode);
+                node.childNodes.splice(i, 1);
+            }
+        }
+        return false;
     }
 }
 
